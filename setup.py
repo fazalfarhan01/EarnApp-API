46a15ca855263551e4d87a2d9635b01a14e6b53a
fazalfarhan01 <fazal.farhan@gmail.com>
2021-11-16 20:47:50 +0530

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
-import codecs
 import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
 VERSION = '0.0.1'
@@ -16,20 +15,22 @@ setup(
     name="pyEarnapp",
     version=VERSION,
     author="fazalfarhan01 (Mohamed Farhan Fazal)",
-    author_email="<fazal@ffehost.com>",
+    author_email="fazal@ffehost.com",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
     install_requires=['requests'],
+    url="https://github.com/fazalfarhan01/EarnApp-API",
+    project_urls={
+        "Bug Tracker": "https://github.com/fazalfarhan01/EarnApp-API/issues",
+    },
     keywords=['python', 'earnapp', 'passive income', 'earnapp api',
               'earnapp dashboard', 'requests', "python earnapp"],
     classifiers=[
         "Development Status :: 1 - Release",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
+        "Operating System :: OS Independent",
     ]
 )
