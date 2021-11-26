5033577aff4fa84776a07902d9f8cd302ab67179
fazalfarhan01 <fazal.farhan@gmail.com>
2021-11-26 10:43:07 +0530

    adding ban details

--- pyEarnapp/earnapp.py
+++ pyEarnapp/earnapp.py
@@ -66,9 +66,21 @@ class RedeemDetails:
             self.email = "No email detected"
             self.payment_method = "No payment method found"
 
+class BanDetails:
+    def __init__(self, ban_info) -> None:
+        if ban_info is False:
+            self.is_banned = False
+            self.reason = None
+            self.ip = None
+            self.details = None
+        else:
+            self.is_banned = True
+            self.reason = ban_info['reason']
+            self.ip = ban_info['ip']
+            self.details = ban_info['details']
 
 class Device:
-    def __init__(self, json_device_info: dict) -> None:
+    def __init__(self, json_device_info: dict):
         self.uuid = json_device_info.get("uuid", "Error retrieving UUID")
         self.bandwidth_usage = json_device_info.get(
             "bw", 0)
@@ -79,14 +91,15 @@ class Device:
         self.rate = json_device_info.get("rate", "Error retrieving rate")
         self.country = json_device_info.get("cn", "UnKnown")
         self.device_type = re.findall('sdk-([a-zA-Z0-9]*)-', self.uuid)[0]
-
+        self.banned = BanDetails(json_device_info.get('banned', False))
 
 class DevicesInfo:
-    def __init__(self, json_devices_info: dict) -> None:
+    def __init__(self, json_devices_info: dict):
         self.devices = []
         self.windows_devices = 0
         self.linux_devices = 0
         self.other_devices = 0
+        self.banned_devices = 0
         self.total_bandwidth_usage = 0
 
         for device in json_devices_info:
@@ -102,6 +115,8 @@ class DevicesInfo:
             else:
                 self.other_devices += 1
             self.total_bandwidth_usage += device.bandwidth_usage
+            if device.banned.is_banned:
+                self.banned_devices += 1
 
 
 class Transaction:
